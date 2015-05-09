399b4ebe41190c59fd37209dfdd9b3629964fef7
Incomplete <incomplete@aixon.co>
2015-05-09 20:23:30 +0800

    modify copy.sh

--- copy.sh
+++ copy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+set -e
+
 REPO=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 to_repo() {
     cp ~/.vimrc $REPO/vimrc
     cp ~/bin/pac.js $REPO/pac.js
+    cp ~/.xinitrc $REPO/xinitrc
+    cp ~/.xmodmap $REPO/xmodmap
 }
 
 to_os() {
-    cp $REPO/.vimrc ~/.vimrc
+    cp $REPO/vimrc ~/.vimrc
     cp $REPO/pac.js ~/bin/pac.js
+    cp $REPO/xinitrc ~/.xinitrc
+    cp $REPO/xmodmap ~/.xmodmap
 }
 
 $1
