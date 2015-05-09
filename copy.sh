ae64f85135949c7daac6cc0386c7887029df2890
Incomplete <incomplete@aixon.co>
2015-05-09 20:18:53 +0800

    add pac.js

--- copy.sh
+++ copy.sh
@@ -3,11 +3,13 @@
 REPO=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 to_repo() {
-    cp ~/.vimrc $REPO/.vimrc
+    cp ~/.vimrc $REPO/vimrc
+    cp ~/bin/pac.js $REPO/pac.js
 }
 
 to_os() {
     cp $REPO/.vimrc ~/.vimrc
+    cp $REPO/pac.js ~/bin/pac.js
 }
 
 $1
